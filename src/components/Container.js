ecf3b88a0faa63834bd4ffa100140ed64933d68d
wyounis25 <github email address>
2020-11-18 20:55:03 -0600

    mapped out components

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Container.css"
+function Container() {
+    return (
+        <div className="container">
+            <h4>Container</h4>
+        </div>
+    )
+}
+
+export default Container
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Search.css"
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <h3>Sidebar</h3>
+        </div>
+    )
+}
+
+export default SideBar
